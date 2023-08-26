cac5e580317d37d76f642d9811fe92d657f89156
Ashish meel <ashishmeel22@gmail.com>
2023-08-26 16:25:27 +0530

    done

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
  require('dotenv').config();
  
-const connection = mongoose.connect("mongodb+srv://ashish:meel@cluster0.3tukwza.mongodb.net/mock4assignmet?retryWrites=true&w=majority");
+const connection = mongoose.connect("mongodb+srv://ashish:meel@cluster0.3tukwza.mongodb.net/mock4assignment?retryWrites=true&w=majority");
 
 module.exports = {
     connection
